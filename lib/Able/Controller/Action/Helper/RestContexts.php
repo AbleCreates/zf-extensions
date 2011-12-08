80277145387fa426eeccdb148411249c9acc71f6
Chris Woodford <chris.woodford@gmail.com>
2011-12-07 20:47:52 -0500

    added functionality for restful web services

---
+++ lib/Able/Controller/Action/Helper/RestContexts.php
@@ -0,0 +1,46 @@
+<?php
+
+class Able_Controller_Action_Helper_RestContexts
+    extends Zend_Controller_Action_Helper_Abstract
+{
+
+    protected $_contexts = array(
+        'xml',
+        'json',
+    );
+
+    public function preDispatch()
+    {
+
+        $controller = $this->getActionController();
+
+        if (!$controller instanceof Zend_Rest_Controller) {
+            return;
+        }
+
+        $this->_initContexts();
+
+        // Set a Vary response header based on the Accept header
+        $this->getResponse()->setHeader('Vary', 'Accept');
+
+    }
+
+    protected function _initContexts()
+    {
+
+        $cs = $this->getActionController()->getHelper('contextSwitch');
+        $cs->setAutoJsonSerialization(false);
+
+        foreach ($this->_contexts as $context) {
+
+            foreach (array('index', 'post', 'get', 'put', 'delete') as $action) {
+                $cs->addActionContext($action, $context);
+            }
+
+        }
+
+        $cs->initContext();
+
+    }
+
+}
