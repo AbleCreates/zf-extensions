917604d68ca29c09969c1efbe7df093e5656db82
Chris Woodford <chris.woodford@gmail.com>
2012-01-11 16:27:41 -0500

    added more decorators to checkbox

--- lib/Able/Form/Bootstrap/Element/Checkbox.php
+++ lib/Able/Form/Bootstrap/Element/Checkbox.php
@@ -15,7 +15,15 @@ class Able_Form_Bootstrap_Element_Checkbox extends Zend_Form_Element_Checkbox
         if (empty($decorators)) {
             $this->addDecorator('ViewHelper')
                  ->addDecorator('Label', array('tag' => 'span'))
-                 ->addDecorator('HtmlTag', array('tag' => 'label'));
+                 ->addDecorator('HtmlTag', array('tag' => 'label'))
+                 ->addDecorator(
+                    array('InputWrapper' => 'HtmlTag'),
+                    array('tag' => 'div', 'class' => 'input')
+                )
+                ->addDecorator(
+                    array('Clearfix' => 'HtmlTag'),
+                    array('tag' => 'div', 'class' => 'clearfix')
+                );
         }
 
         return $this;
