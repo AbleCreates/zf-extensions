d04870f48707d71b8ec465e12bfddf538dbaadbd
Chris Woodford <chris.woodford@gmail.com>
2012-01-29 23:32:57 -0500

    added a sub form

---
+++ lib/Able/Form/Bootstrap/SubForm.php
@@ -0,0 +1,31 @@
+<?php
+
+class Able_Form_Bootstrap_SubForm extends Able_Form_Bootstrap_Form
+{
+    /**
+     * Whether or not form elements are members of an array
+     * @var bool
+     */
+    protected $_isArray = true;
+
+    /**
+     * Load the default decorators
+     *
+     * @return Zend_Form_SubForm
+     */
+    public function loadDefaultDecorators()
+    {
+        if ($this->loadDefaultDecoratorsIsDisabled()) {
+            return $this;
+        }
+
+        $decorators = $this->getDecorators();
+        if (empty($decorators)) {
+            $this->addDecorator('FormElements')
+                 ->addDecorator('Fieldset');;
+        }
+
+        return $this;
+
+    }
+}
